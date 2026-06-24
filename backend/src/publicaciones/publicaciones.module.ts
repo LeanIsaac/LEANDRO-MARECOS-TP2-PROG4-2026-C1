e10360b4935b54f0bd825682fe706ab92eaf7baf
LeanIsaac <leancarp09@gmail.com>
2026-06-24 00:37:24 -0300

    feat: defino los servicios que voy a usar

--- backend/src/publicaciones/publicaciones.module.ts
+++ backend/src/publicaciones/publicaciones.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PublicacionesService } from './publicaciones.service';
 import { PublicacionesController } from './publicaciones.controller';
+import { Publicacion, PublicacionSchema } from './entities/publicacion.schema';
+import { CloudinaryModule } from '../cloudinary/cloudinary.module'; // Lo importamos para poder subir fotos de posts
+import { AutenticacionModule } from 'src/autenticacion/autenticacion.module';
 
 @Module({
+  imports: [
+    // Registramos el modelo en Mongoose
+    MongooseModule.forFeature([
+      { name: Publicacion.name, schema: PublicacionSchema },
+    ]),
+    CloudinaryModule,
+    AutenticacionModule,
+  ],
   controllers: [PublicacionesController],
   providers: [PublicacionesService],
+  exports: [PublicacionesService], // Por si otro módulo necesita leer publicaciones
 })
 export class PublicacionesModule {}

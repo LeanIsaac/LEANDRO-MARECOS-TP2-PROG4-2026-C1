4b2008237589edc1fa47f584750489dac01f368a
LeanIsaac <leancarp09@gmail.com>
2026-06-29 23:32:47 -0300

    feat: agrega ruta detalle-publicacion

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,5 +6,6 @@ export const routes: Routes = [
 { path: 'registro', loadComponent: () => import('./components/registro/registro').then(m => m.Registro) },
 { path: 'mi-perfil', loadComponent: () => import('./components/mi-perfil/mi-perfil').then(m => m.MiPerfil) },
 { path: 'publicaciones', loadComponent: () => import('./components/publicaciones/publicaciones').then(m => m.Publicaciones) },
+{ path: 'publicaciones/:id', loadComponent: () => import('./components/detalle-publicacion/detalle-publicacion').then(m => m.DetallePublicacion) },
 { path: '**', redirectTo: '' } // en caso de que no se encuentre ninguna ruta, se redirige al login
 ];
